a54807e1af9a065106bafd526aadc1ae40d5c743
Pavel Sturov <pavel.sturov@winlinetech.net>
2022-09-23 18:03:32 +0300

    Added dependencies.

--- apps/account/jest.config.ts
+++ apps/account/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'accounts',
+  displayName: 'account',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/accounts',
+  coverageDirectory: '../../coverage/apps/account',
 };
